079943abc55b2491b4249c8f63a1e22334281ef6
OlgaGraf0 <olgagrafov@gmail.com>
2023-01-24 17:12:06 +0200

    added webview

--- app/src/main/java/com/olgag/qrcreator/MainActivity.kt
+++ app/src/main/java/com/olgag/qrcreator/MainActivity.kt
@@ -4,14 +4,16 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.BorderStroke
@@ -25,6 +27,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -34,13 +37,13 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.FileProvider
 import com.olgag.qrcreator.ui.theme.QRCreatorTheme
 import com.olgag.qrcreator.viewmodel.QRViewModel
@@ -54,18 +57,19 @@ const val MAX_CHAR = 2048
 const val MAX_LINES_FOR_INPUT_TEXT = 3
 const val MIN_LENGTH_QR_TEXT = 10
 
+var qrFromURL:String = ""
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-           // Text(text =" stringResource(R.string.create)")
             QRCreatorTheme {
-                GetScaffold(this)
+               GetScaffold(this)
             }
         }
     }
 }
+
 
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     @Composable
@@ -80,7 +84,8 @@ class MainActivity : ComponentActivity() {
                         painter = painterResource(R.drawable.ic_qr_code),
                         contentDescription = "",
                         contentScale = ContentScale.Crop,
-                        modifier = Modifier.padding(5.dp)
+                        modifier = Modifier
+                            .padding(5.dp)
                             .size(50.dp)
                             .clip(CircleShape)
                             .border(2.dp, MaterialTheme.colors.background, CircleShape)
@@ -99,49 +104,42 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MainContent(context: Context) {
-        val configuration  = LocalConfiguration.current
-        val isPortrait by remember { mutableStateOf(
-            when (configuration.orientation) {
-                Configuration.ORIENTATION_LANDSCAPE -> {
-                    false
-                }
-                else -> {
-                    true
-                }
-            }
-        )}
         Box(
             Modifier
-                .background(brush = Brush.verticalGradient(
-                    colors = listOf(
-                        MaterialTheme.colors.background,
-                        MaterialTheme.colors.primary
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            MaterialTheme.colors.background,
+                            MaterialTheme.colors.primary
+                        )
                     )
-                ))
+                )
                 .fillMaxSize()
                 .wrapContentSize(Alignment.TopCenter))
-                { SaveableContent(isPortrait, context) }
+                { SaveableContent(context) }
     }
 
     @Composable
-    fun SaveableContent(isPortrait: Boolean, context: Context) {
+    fun SaveableContent(context: Context) {
         var qrText by rememberSaveable { mutableStateOf("") }
         QRView(context, qrText = qrText,
-            onQrTextChange = { if (it.length <= MAX_CHAR) qrText = it },
-            isPortrait = isPortrait)
+            onQrTextChange = { if (it.length <= MAX_CHAR) qrText = it })
     }
 
     @OptIn(ExperimentalComposeUiApi::class)
     @Composable
-    fun QRView(context: Context, qrText: String, onQrTextChange: (String) -> Unit, isPortrait: Boolean) {
+    fun QRView(context: Context, qrText: String, onQrTextChange: (String) -> Unit) {
         val btTmp:Bitmap?=null
-        var pic by rememberSaveable { mutableStateOf(btTmp) }
+        var pic by remember { mutableStateOf(btTmp) }
+        var isShowGoogle by remember { mutableStateOf(false) }
         val kc = LocalSoftwareKeyboardController.current
         val innerPadding = PaddingValues(start = 20.dp)
 
+        var qrText1 = if (qrFromURL.isNotBlank()) qrFromURL else qrText
         val trailingIconViewClean = @Composable {
             IconButton(
                 onClick = {
+                    qrFromURL = ""
                     onQrTextChange("")
                     pic = null
                 },
@@ -151,21 +149,37 @@ class MainActivity : ComponentActivity() {
         }
 
         Column(modifier = Modifier.padding(16.dp)) {
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(70.dp)
+                    .padding(vertical = 5.dp),
+                shape = MaterialTheme.shapes.medium,
+                onClick = { isShowGoogle =! isShowGoogle }) {
+                Text(if(isShowGoogle) context.getString(R.string.close_google) else context.getString(R.string.google_url))
+            }
+
+            if (isShowGoogle) {
+                isShowGoogle = WebViewPage(context)
+            }
+
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement  = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 OutlinedTextField(
-                    modifier = if(isPortrait) Modifier.fillMaxWidth().weight(1f)
-                               else  Modifier.width(350.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
                     shape = MaterialTheme.shapes.medium,
-                    value = qrText,
+                    value = qrText1,
                     leadingIcon = {
                        Icon(painterResource(R.drawable.ic_qr_img), null,
-                           Modifier.clip(CircleShape)
+                           Modifier
+                               .clip(CircleShape)
                                .border(1.dp, MaterialTheme.colors.surface, CircleShape))
                     },
-                    trailingIcon = if (qrText.isNotBlank()) trailingIconViewClean else null,
+                    trailingIcon = if (qrText1.isNotBlank()) trailingIconViewClean else null,
                     onValueChange = onQrTextChange,
                     label = { Text(text = stringResource(R.string.text_qr)) },
                     placeholder = {
@@ -176,23 +190,28 @@ class MainActivity : ComponentActivity() {
                     },
                     maxLines = MAX_LINES_FOR_INPUT_TEXT,
                 )
-             //   if(qrText.trim().length > MIN_LENGTH_QR_TEXT) {
-                //    if (isUrl(qrText)) {
+                if(qrText1.trim().length > MIN_LENGTH_QR_TEXT) {
+                    if (isUrl(qrText1)) {
                         IconButton(
-                            modifier = Modifier.width(50.dp).padding(innerPadding),
+                            modifier = Modifier
+                                .width(50.dp)
+                                .padding(innerPadding),
                             onClick = {
-                                pic = createBitmapFromText(qrText, context)
+                                pic = createBitmapFromText(qrText1, context)
                                 kc?.hide()
                             },
                         ) {
                             Icon(Icons.Default.Done, null)
                         }
                     }
-               // }
-           // }
+                }
+            }
+
             pic?.let {
                 Row(
-                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
                     horizontalArrangement  = Arrangement.Center) {
                     Column(
                         modifier = Modifier.fillMaxWidth(),
@@ -243,9 +262,50 @@ class MainActivity : ComponentActivity() {
         )
     }
 
-    fun createBitmapFromText(qRStrin: String ,context: Context): Bitmap {
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebViewPage(context: Context) : Boolean{
+    var backEnabled by remember { mutableStateOf(false) }
+    var webView by remember { mutableStateOf<WebView?>(null) }
+    var returnValue by remember { mutableStateOf(true) }
+
+    AndroidView(
+        modifier = Modifier.padding(5.dp).height(400.dp),
+        factory = {
+            WebView(it).apply {
+                webViewClient = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                        backEnabled = view.canGoBack()
+                    }
+                }
+                settings.javaScriptEnabled = true
+                webView = this
+            }
+        }, update = {
+            webView = it
+            webView?.loadUrl(context.getString(R.string.start_url))
+        }
+    )
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
+            .padding(vertical = 5.dp),
+        shape = MaterialTheme.shapes.medium,
+        onClick = { qrFromURL = webView?.url.toString()
+                    returnValue = false
+            }) {
+        Text(context.getString(R.string.set_url))
+    }
+    BackHandler(enabled = backEnabled) {
+        webView?.goBack()
+    }
+    return returnValue
+}
+
+fun createBitmapFromText(qRString: String ,context: Context): Bitmap {
         val viewModel = QRViewModel(context)
-        viewModel.qrText = qRStrin
+        viewModel.qrText = qRString
         return  viewModel.CreateBitmapQR()
     }
 
@@ -275,7 +335,7 @@ class MainActivity : ComponentActivity() {
 
         } catch (e: Exception) {
             Toast.makeText(context, context.getString(R.string.wrong), Toast.LENGTH_SHORT).show()
-            Log.i("e_catch:", e.toString())
+           // Log.i("e_catch:", e.toString())
         }
     }
     fun isUrl(string: String): Boolean {
